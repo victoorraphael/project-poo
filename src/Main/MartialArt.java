b051d36cb23b0e567314fed73d48276a3ac27c83
Raffael César <raffaelcsar@gmail.com>
2020-08-28 21:40:02 -0300

    Create MartialArt Class

---
+++ src/Main/MartialArt.java
@@ -0,0 +1,40 @@
+package Main;
+
+public class MartialArt {
+
+	private String jiuJitsu;
+	private String muayThai;
+	private String kickBox;
+
+	public MartialArt(String jiuJitsu, String muayThai, String kickBox) {
+
+		this.jiuJitsu = jiuJitsu;
+		this.muayThai = muayThai;
+		this.kickBox = kickBox;
+	}
+
+	public String getJiuJitsu() {
+		return jiuJitsu;
+	}
+
+	public void setJiuJitsu(String jiuJitsu) {
+		this.jiuJitsu = jiuJitsu;
+	}
+
+	public String getMuayThai() {
+		return muayThai;
+	}
+
+	public void setMuayThai(String muayThai) {
+		this.muayThai = muayThai;
+	}
+
+	public String getKickBox() {
+		return kickBox;
+	}
+
+	public void setKickBox(String kickBox) {
+		this.kickBox = kickBox;
+	}
+
+}
